6741beab98550aaf2e462beab3bcc5c5e5b4f27e
h3xsniper0x <hexsniper0x@gmail.com>
2026-01-22 23:48:49 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,13 @@ python manage.py collectstatic --noinput
 
 # Apply database migrations
 python manage.py migrate
+# إنشاء Superuser تلقائياً إذا لم يكن موجوداً
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', '782007038~!@')
+    print('Superuser created successfully')
+else:
+    print('Superuser already exists')
+EOF
